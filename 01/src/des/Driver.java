3151b43149821547d6895c8c0ad72a6f62c57991
ochman2000@gmail.com <ochman2000@gmail.com@051e7f4d-2495-7d2a-f62f-c224edca2474>
2014-03-13 01:09:54 +0000

    Utworzony key object

--- 01/src/des/Driver.java
+++ 01/src/des/Driver.java
@@ -4,18 +4,16 @@ public class Driver {
 
 	public static void main(String[] args) {
 		
-		String message = "hahaha";
-		String hex = "686168616861";
-		
+		String mHex = "0123456789ABCDEF";
+		String kHex = "133457799BBCDFF1";
+		Key key = new Key();
+		key.setHexText(kHex);
 		Input input = new Input();
-		input.setPlainText(message);
-		System.out.println(input.getHexRepresentation());
-		System.out.println(input.getBitRepresentation());
-
-		input.setHexText(hex);
-		System.out.println(input.getHexRepresentation());
-		System.out.println(input.getBitRepresentation());
+		input.setHexText(mHex);
+		System.out.println(key.getHexRepresentation());
+		System.out.println(key.getBitRepresentation());
 		
+		Output output = Utils.encode(input, key);
 		
 	}
 }
