2eea371e8166cda8826952d83cb9bad2a4ea482c
ochman2000@gmail.com <ochman2000@gmail.com@051e7f4d-2495-7d2a-f62f-c224edca2474>
2014-03-14 20:00:03 +0000

    Zastosowanie autorskiej metody dziedziczenia BitSet poprzez schowek. Teraz już nie używamy klasy java.utils.BitSet tylko mojej z des.BitSet, która zawiera dokładnie to samo, plus 4 metody ekstra

--- 01/src/des/Driver.java
+++ 01/src/des/Driver.java
@@ -1,18 +1,16 @@
 package des;
 
-import java.util.BitSet;
-
 public class Driver {
 	
 	public static void main(String[] args) {
 		
 		String mHex = "0123456789ABCDEF";
 		String kHex = "133457799BBCDFF1";
-		BitSet key = Key.setHexText(kHex);
+		BitSet key = BitSet.setHexText(kHex);
 //		Input input = new Input();
 //		input.setHexText(mHex);
-//		System.out.println(Key.getHexRepresentation(key));
-		System.out.println(Key.getBitRepresentation(key));
+//		System.out.println(key.getHexRepresentation());
+		System.out.println(key.getBitRepresentation());
 //		Output output = Utils.encode(input, key);
 
 		new Encoder().step1(key);
