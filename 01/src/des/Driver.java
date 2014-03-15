ec762026754eae097c4fa66d9e12ce3bb127ba07
ochman2000@gmail.com <ochman2000@gmail.com@051e7f4d-2495-7d2a-f62f-c224edca2474>
2014-03-15 09:16:52 +0000

    Teraz jest poprawione wyświetlanie bitów.

--- 01/src/des/Driver.java
+++ 01/src/des/Driver.java
@@ -1,5 +1,7 @@
 package des;
 
+import java.math.BigInteger;
+
 public class Driver {
 	
 	public static void main(String[] args) {
@@ -7,6 +9,8 @@ public class Driver {
 		String mHex = "0123456789ABCDEF";
 		String kHex = "133457799BBCDFF1";
 		BitSet key = BitSet.valueOfHex(kHex);
+		BigInteger key2 = new BigInteger(kHex, 16);
+		System.out.println(key2.toString(2));
 //		Input input = new Input();
 //		input.setHexText(mHex);
 //		System.out.println(key.getHexRepresentation());
