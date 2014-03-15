0e8c5cec1aae04e6ba83179e30ba464e769fbd0b
ochman2000@gmail.com <ochman2000@gmail.com@051e7f4d-2495-7d2a-f62f-c224edca2474>
2014-03-15 20:54:23 +0000

    Tym razem operacja na BitSet powiodła się.

--- trunk/01/src/des/Driver.java
+++ trunk/01/src/des/Driver.java
@@ -8,29 +8,10 @@ public class Driver {
 		
 		String mHex = "0123456789ABCDEF";
 		String kHex = "133457799BBCDFF1";
-		BitSet key = BitSet.valueOfHex(kHex);
-		BigInteger key2 = new BigInteger(kHex, 16);
-		
-		if (Encoder.DEBUG) {
-			System.out.println("Poniżej klucz BigInteger bez pierwszych trzech zer:");
-			System.out.println(key2.toString(2));
-			System.out.println("Poniżej jest iteracja tego badziewia z użyciem BigInteger:");
-			for (int i=0; i<key.length(); i++) {
-				if (key2.testBit(i))
-					System.out.print(1);
-				else
-					System.out.print(0);
-				if (i%8==7)
-					System.out.print(" ");
-			}
-			System.out.println();
-		}
-//		Input input = new Input();
-//		input.setHexText(mHex);
-//		System.out.println(key.getHexRepresentation());
-		System.out.println("Poniżej moja metoda BitSet.getBitRepresentation:");
-		System.out.println(key.getBitRepresentation());
-//		Output output = Utils.encode(input, key);
+		byte[] kBits = {
+				0,0,0,1,0,0,1,1,	0,0,1,1,0,1,0,0,	0,1,0,1,0,1,1,1,	0,1,1,1,1,0,0,1,
+				1,0,0,1,1,0,1,1,	1,0,1,1,1,1,0,0,	1,1,0,1,1,1,1,1,	1,1,1,1,0,0,0,1};
+		BitSet key = BitSet.valueOfBits(kBits);
 
 		new Encoder().step1(key);
 	}
