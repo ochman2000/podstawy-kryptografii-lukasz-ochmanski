7b686934345c2481c7c98001583726abaa4859f5
ochman2000@gmail.com <ochman2000@gmail.com@051e7f4d-2495-7d2a-f62f-c224edca2474>
2014-04-06 11:23:29 +0000

    Teraz śmiga jak ta lala

--- trunk/02/src/pl/lodz/p/pk/Encoder.java
+++ trunk/02/src/pl/lodz/p/pk/Encoder.java
@@ -9,29 +9,21 @@ public class Encoder {
 	public static Debug LEVEL = Debug.LEVEL1;
 
 	public Encoder() {
-		
-		
+
+	}
+
+	public BigInteger szyfruj(BigInteger msg, Key klucz) {
+		return msg.modPow(klucz.getA(), klucz.getN());
 	}
 	
-	public BigInteger[] szyfruj(byte[] message, Key klucz) {
-		int liczbaZnakow = klucz.getLiczbaZnakow();
-		int chunks = message.length / liczbaZnakow;
-		BigInteger[] szyfrogram = new BigInteger[chunks];
-		for (int i = 0; i < chunks; i++) {
-			int from = liczbaZnakow*i;
-			int to = liczbaZnakow*(i + 1);
-			byte[] pom = Auxx.podtablica(message, from, to);
-			szyfrogram[i] = new BigInteger(1, pom);
-			szyfrogram[i] = szyfrogram[i].modPow(klucz.getA(), klucz.getN());
-		}
-		return szyfrogram;
+	public BigInteger deszyfruj(BigInteger msg, Key klucz) {
+		return msg.modPow(klucz.getA(), klucz.getN());
 	}
-	
-    public BigInteger[] decryptToBigInt(BigInteger[] cipher, Key klucz) 
-    {
-      BigInteger[] wynik = new BigInteger[cipher.length];
-      for (int i = 0; i < cipher.length; i++) 
-          wynik[i] = cipher[i].modPow(klucz.getA(), klucz.getN());
-      return wynik;
-    }
+
+	public BigInteger[] decryptToBigInt(BigInteger[] cipher, Key klucz) {
+		BigInteger[] wynik = new BigInteger[cipher.length];
+		for (int i = 0; i < cipher.length; i++)
+			wynik[i] = cipher[i].modPow(klucz.getA(), klucz.getN());
+		return wynik;
+	}
 }
