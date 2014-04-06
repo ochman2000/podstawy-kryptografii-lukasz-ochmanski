d0b07ee3b01000e76b28cd6982e96bd4d641ef77
ochman2000@gmail.com <ochman2000@gmail.com@051e7f4d-2495-7d2a-f62f-c224edca2474>
2014-04-06 11:14:01 +0000

    Tu coś nie bardzo działa.

--- 02/src/pl/lodz/p/pk/Driver.java
+++ 02/src/pl/lodz/p/pk/Driver.java
@@ -1,5 +1,7 @@
 package pl.lodz.p.pk;
 
+import java.math.BigInteger;
+
 public class Driver {
 	
 	public static void main(String[] args) {
@@ -7,11 +9,23 @@ public class Driver {
 		new MainFrame();
 
 		//A MESSAGE
-		String tekst = "01234567";
+		String tekst = "1";
 
 		RSAKey klucz  = new RSAKey();
 		System.out.println("Klucz prywatny: "+ klucz.getPrywatny());
 		System.out.println("Klucz publiczny: "+ klucz.getPubliczny());
 
+		Encoder en = new Encoder();
+		BigInteger[] szyfrogram = en.szyfruj(tekst.getBytes(), klucz.getPrywatny());
+		System.out.println("Plain tekst: "+ tekst);
+		
+		BigInteger[] tekstb = new BigInteger[] {new BigInteger(tekst)};
+		System.out.println("Plain tekst: "+ tekstb);
+		
+		System.out.println("Szyfrogram:  "+ szyfrogram.toString());
+		
+		BigInteger[] plainTekst = en.decryptToBigInt(szyfrogram, klucz.getPubliczny());
+		System.out.println("Odszyfrowany: "+ plainTekst);
+		
 	}
 }
--- 02/src/pl/lodz/p/pk/Encoder.java
+++ 02/src/pl/lodz/p/pk/Encoder.java
@@ -1,5 +1,9 @@
 package pl.lodz.p.pk;
 
+import java.math.BigInteger;
+
+import pl.lodz.p.tewi.Auxx;
+
 public class Encoder {
 
 	public static Debug LEVEL = Debug.LEVEL1;
@@ -9,8 +13,25 @@ public class Encoder {
 		
 	}
 	
-	public byte[] szyfruj(byte[] a, Key klucz) {
-		return a;
-		
+	public BigInteger[] szyfruj(byte[] message, Key klucz) {
+		int liczbaZnakow = klucz.getLiczbaZnakow();
+		int chunks = message.length / liczbaZnakow;
+		BigInteger[] szyfrogram = new BigInteger[chunks];
+		for (int i = 0; i < chunks; i++) {
+			int from = liczbaZnakow*i;
+			int to = liczbaZnakow*(i + 1);
+			byte[] pom = Auxx.podtablica(message, from, to);
+			szyfrogram[i] = new BigInteger(1, pom);
+			szyfrogram[i] = szyfrogram[i].modPow(klucz.getA(), klucz.getN());
+		}
+		return szyfrogram;
 	}
+	
+    public BigInteger[] decryptToBigInt(BigInteger[] cipher, Key klucz) 
+    {
+      BigInteger[] wynik = new BigInteger[cipher.length];
+      for (int i = 0; i < cipher.length; i++) 
+          wynik[i] = cipher[i].modPow(klucz.getA(), klucz.getN());
+      return wynik;
+    }
 }
