c80bc893b3ee757a177d2d16a6a910208ebca2ef
ochman2000@gmail.com <ochman2000@gmail.com@051e7f4d-2495-7d2a-f62f-c224edca2474>
2014-04-06 15:49:42 +0000

    Odszyfrowywanie pliku działa.

--- trunk/02/src/pl/lodz/p/pk/Driver.java
+++ trunk/02/src/pl/lodz/p/pk/Driver.java
@@ -7,23 +7,6 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		new MainFrame();
-
-		//A MESSAGE
-		Encoder en = new Encoder();
-		RSAKey klucz  = new RSAKey();
-		System.out.println("Klucz prywatny: "+ klucz.getPrywatny());
-		System.out.println("Klucz publiczny: "+ klucz.getPubliczny());
-		
-		String tekst1 = "HAHHAH";
-		System.out.println("Plain tekst: "+ tekst1);
-		
-		String szyfrogram = en.szyfruj(tekst1, klucz.getPrywatny());
-		System.out.println("Zaszyfrowany:  "+ szyfrogram);
-		
-		String tekst2 = en.deszyfruj(szyfrogram, klucz.getPubliczny());
-		System.out.println("Odszyfrowany: "+ tekst2);
-		
-		assertTrue(tekst1+".equals("+tekst2+")", tekst1.equals(tekst2));
 		
 	}
 }
